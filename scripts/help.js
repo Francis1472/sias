fe1c34a01696bc6ae6d492e2987d024b6aeab8a6
@04 <alfrancis308@gmail.com>
2023-05-19 22:46:01 +0800

    update

---
+++ scripts/help.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    const Sp = document.getElementById("Sp");
+
+});
--- scripts/transactions.js
+++ scripts/transactions.js
@@ -6,7 +6,12 @@ $(document).ready(function () {
     const Ace = document.getElementById("Ace");
     const Assessment = document.getElementById("Assessment"); 
     const Lm = document.getElementById("Lm");
-    const Mot = document.getElementById("Mot");         
+    const Mot = document.getElementById("Mot"); 
+    const Mts = document.getElementById("Mts"); 
+    const Te = document.getElementById("Te");                 
+    const Dic = document.getElementById("Dic"); 
+    const Opd = document.getElementById("Opd");   
+    const Ep = document.getElementById("Ep");                             
 
     Sp.style.display = "block";
     // Sp.style.display = "none";
@@ -16,7 +21,12 @@ $(document).ready(function () {
     Ace.style.display = "none";  
     Assessment.style.display = "none";  
     Lm.style.display = "none";   
-    Mot.style.display = "none";          
+    Mot.style.display = "none"; 
+    Mts.style.display = "none";  
+    Te.style.display = "none"; 
+    Dic.style.display = "none";
+    Opd.style.display = "none";  
+    Ep.style.display = "none";                              
 
     const bSp = document.getElementById("bSp");
     const bCp = document.getElementById("bCp");
@@ -25,11 +35,21 @@ $(document).ready(function () {
     const bAce = document.getElementById("bAce");
     const bAssessment = document.getElementById("bAssessment");  
     const bLm = document.getElementById("bLm"); 
-    const bMot = document.getElementById("bMot");            
-
-    $("#bMot").click(function (e) {
+    const bMot = document.getElementById("bMot"); 
+    const bMts = document.getElementById("bMts"); 
+    const bTe = document.getElementById("bTe");   
+    const bDic = document.getElementById("bDic");  
+    const bOpd = document.getElementById("bOpd");     
+    const bEp = document.getElementById("bEp");                                    
+                 
+    $("#bEp").click(function (e) {
         e.preventDefault();
-        Mot.style.display = "block";          
+        Ep.style.display = "block";
+        Opd.style.display = "none";
+        Dic.style.display = "none";                      
+        Te.style.display = "none";
+        Mts.style.display = "none";              
+        Mot.style.display = "none";          
         Lm.style.display = "none";      
         Ace.style.display = "none";  
         Assessment.style.display = "none";
@@ -39,6 +59,90 @@ $(document).ready(function () {
         Sp.style.display = "none";
     });
 
+    $("#bOpd").click(function (e) {
+        e.preventDefault();
+        Opd.style.display = "block";
+        Dic.style.display = "none";                      
+        Te.style.display = "none";
+        Mts.style.display = "none";              
+        Mot.style.display = "none";          
+        Lm.style.display = "none";      
+        Ace.style.display = "none";  
+        Assessment.style.display = "none";
+        Enrollment.style.display = "none";
+        Pe.style.display = "none";
+        Cp.style.display = "none";
+        Ep.style.display = "none";
+        Sp.style.display = "none";
+    });
+
+    $("#bDic").click(function (e) {
+        e.preventDefault();
+        Dic.style.display = "block";                      
+        Te.style.display = "none";
+        Mts.style.display = "none";        
+        Opd.style.display = "none";      
+        Mot.style.display = "none";     
+        Ep.style.display = "none";     
+        Lm.style.display = "none";      
+        Ace.style.display = "none";  
+        Assessment.style.display = "none";
+        Enrollment.style.display = "none";
+        Pe.style.display = "none";
+        Cp.style.display = "none";
+        Sp.style.display = "none";
+    });
+
+    $("#bTe").click(function (e) {
+        e.preventDefault();
+        Te.style.display = "block";
+        Mts.style.display = "none";              
+        Mot.style.display = "none";          
+        Lm.style.display = "none";      
+        Ep.style.display = "none";
+        Opd.style.display = "none";
+        Ace.style.display = "none";  
+        Dic.style.display = "none";
+        Assessment.style.display = "none";
+        Enrollment.style.display = "none";
+        Pe.style.display = "none";
+        Cp.style.display = "none";
+        Sp.style.display = "none";
+    });
+
+    $("#bMts").click(function (e) {
+        e.preventDefault();
+        Mts.style.display = "block";              
+        Mot.style.display = "none";          
+        Lm.style.display = "none";    
+        Opd.style.display = "none";
+        Ep.style.display = "none";
+        Te.style.display = "none";  
+        Dic.style.display = "none";
+        Ace.style.display = "none";  
+        Assessment.style.display = "none";
+        Enrollment.style.display = "none";
+        Pe.style.display = "none";
+        Cp.style.display = "none";
+        Sp.style.display = "none";
+    });
+
+    $("#bMot").click(function (e) {
+        e.preventDefault();
+        Mot.style.display = "block";          
+        Lm.style.display = "none";      
+        Ace.style.display = "none";  
+        Assessment.style.display = "none";
+        Enrollment.style.display = "none";
+        Pe.style.display = "none";
+        Dic.style.display = "none";
+        Cp.style.display = "none";
+        Ep.style.display = "none";
+        Opd.style.display = "none";
+        Sp.style.display = "none";
+        Te.style.display = "none";
+    });
+
     $("#bLm").click(function (e) {
         e.preventDefault();
         Lm.style.display = "block";      
@@ -46,6 +150,11 @@ $(document).ready(function () {
         Assessment.style.display = "none";
         Enrollment.style.display = "none";
         Mot.style.display = "none";
+        Mts.style.display = "none";
+        Ep.style.display = "none";
+        Opd.style.display = "none";
+        Dic.style.display = "none";
+        Te.style.display = "none";
         Pe.style.display = "none";
         Cp.style.display = "none";
         Sp.style.display = "none";
@@ -59,6 +168,11 @@ $(document).ready(function () {
         Pe.style.display = "none";
         Lm.style.display = "none";
         Mot.style.display = "none";
+        Te.style.display = "none";
+        Dic.style.display = "none";
+        Ep.style.display = "none";
+        Opd.style.display = "none";
+        Mts.style.display = "none";
         Cp.style.display = "none";
         Sp.style.display = "none";
     });
@@ -69,7 +183,12 @@ $(document).ready(function () {
         Ace.style.display = "none";
         Enrollment.style.display = "none";
         Pe.style.display = "none";
+        Opd.style.display = "none";
+        Te.style.display = "none";
+        Ep.style.display = "none";
         Mot.style.display = "none";
+        Dic.style.display = "none";
+        Mts.style.display = "none";
         Lm.style.display = "none";
         Cp.style.display = "none";
         Sp.style.display = "none";
@@ -82,7 +201,12 @@ $(document).ready(function () {
         Mot.style.display = "none";
         Assessment.style.display = "none";
         Lm.style.display = "none";
+        Mts.style.display = "none";
+        Te.style.display = "none";
         Ace.style.display = "none";    
+        Ep.style.display = "none";
+        Opd.style.display = "none";
+        Dic.style.display = "none";
         Cp.style.display = "none";
         Sp.style.display = "none";
     });
@@ -92,8 +216,13 @@ $(document).ready(function () {
         Pe.style.display = "block";
         Cp.style.display = "none";
         Enrollment.style.display = "none"; 
+        Dic.style.display = "none";
         Lm.style.display = "none";   
+        Opd.style.display = "none";
+        Ep.style.display = "none";
+        Te.style.display = "none";
         Sp.style.display = "none";
+        Mts.style.display = "none";
         Mot.style.display = "none";
         Assessment.style.display = "none";
         Ace.style.display = "none";    
@@ -107,6 +236,11 @@ $(document).ready(function () {
         Assessment.style.display = "none";
         Mot.style.display = "none";
         Lm.style.display = "none";
+        Opd.style.display = "none";
+        Ep.style.display = "none";
+        Mts.style.display = "none";
+        Dic.style.display = "none";
+        Te.style.display = "none";
         Pe.style.display = "none";
         Enrollment.style.display = "none";    
     });
@@ -116,9 +250,14 @@ $(document).ready(function () {
         Sp.style.display = "block";
         Cp.style.display = "none";
         Mot.style.display = "none";
+        Ep.style.display = "none";
         Ace.style.display = "none";    
+        Opd.style.display = "none";
         Assessment.style.display = "none";
+        Te.style.display = "none";
         Lm.style.display = "none";
+        Dic.style.display = "none";
+        Mts.style.display = "none";
         Pe.style.display = "none";
         Enrollment.style.display = "none";    
     });
